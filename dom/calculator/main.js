f6282efd2101651c5a1876ce59340ab897684bf6
Ninti Chance <ninhursag_chance@homedepot.com>
2020-07-13 12:17:48 -0400

    Fix bug that doesn't allow new calculations

--- dom/calculator/main.js
+++ dom/calculator/main.js
@@ -1,13 +1,18 @@
 const buttons = document.querySelector('.button-wrapper')
+const input = document.querySelector('input')
 const userInputs = []
 const numbers = []
-const input = document.querySelector('input')
 
 buttons.addEventListener('click', (event)=>{
+  console.log(event)
   if(event.target.textContent === '=') {
     userInputs.push(numbers.join())
     numbers.splice(0, numbers.length)
     input.value = performOperations(userInputs[1], parseInt(userInputs[0]), parseInt(userInputs[2]))
+  } else if (event.target.textContent === 'C') {
+    input.value=''
+    userInputs.splice(0, userInputs.length)
+    numbers.splice(0, numbers.length)
   } else if (event.target.className === 'operator') {
     userInputs.push(numbers.join())
     userInputs.push(event.target.textContent)
@@ -16,13 +21,10 @@ buttons.addEventListener('click', (event)=>{
   } else if (event.target.className === 'num') {
     input.value+=event.target.textContent
     numbers.push(event.target.textContent)
-  } else if (event.target.id === 'clear') {
-    input.value=''
-  }
+  } 
 })
 
 const performOperations = (operator, num1, num2) => {
-  console.log(operator, num1, num2)
   if (operator === '+') {
     return num1 + num2
   } else if (operator === '-') {
